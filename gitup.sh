efb49f5f527f7ea9a7f4b098c404eec60357c515
barbel-thierry <38921069+barbel-thierry@users.noreply.github.com>
2025-03-28 09:50:18 +0100

    check on master or main branch

--- gitup.sh
+++ gitup.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-SIMPLE_FOLDER=()
+NON_GIT_REPO=()
+NON_MASTER_BRANCH=()
 
-for folder in */
-do
-  if [ -e "$folder/.git" ]; then
+for folder in */; do
+  if [[ -e "$folder/.git" ]]; then
+    branch="$(git -C "$folder" branch --show-current)"
+
+    if [[ "$branch" != "master" && "$branch" != "main" ]]; then
+      NON_MASTER_BRANCH+=("$folder")
+      continue
+    fi
+
     echo "$folder"
     git -C "$folder" pull --rebase
   else
-    SIMPLE_FOLDER+=("$folder")
+    NON_GIT_REPO+=("$folder")
   fi
 done
 
-echo "These are not GIT repositories: ${SIMPLE_FOLDER[*]}"
+if [[ ${#NON_GIT_REPO[@]} -ne 0 ]]; then
+    echo "These are not GIT repositories: ${NON_GIT_REPO[*]}"
+fi
+
+if [[ ${#NON_MASTER_BRANCH[@]} -ne 0 ]]; then
+    echo "These are not on master/main branch: ${NON_MASTER_BRANCH[*]}"
+fi
